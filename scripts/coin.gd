1ab6e0d87c3796e80c9c2d918a39811304f3b6c4
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-07-02 18:45:47 -0300

    Implementação da música e do SFX da moeda

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,8 +2,15 @@ extends Area2D
 
 
 @onready var game_manager: Node = %GameManager
+@onready var sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
+	sound.play()
+	visible = false
+	$CollisionShape2D.queue_free()
+
+
+func _on_audio_stream_player_2d_finished() -> void:
 	queue_free()
