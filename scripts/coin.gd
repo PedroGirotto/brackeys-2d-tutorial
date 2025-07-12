258c5bbbfacf3b3d33ea8a7214b9edbeb3704b36
PedroGirotto <pedro.hs.girotto@gmail.com>
2025-07-12 13:14:13 -0300

    Atualizando a função de atualizar o texto do score

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,7 +6,6 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_point()
 	sound.play()
 	visible = false
 	$CollisionShape2D.queue_free()
@@ -14,3 +13,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_audio_stream_player_2d_finished() -> void:
 	queue_free()
+
+
+func _on_tree_exited() -> void:
+	game_manager.add_point()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,8 +3,13 @@ extends Node
 
 var score:int = 0
 @onready var score_text: Label = $"../CanvasLayer/AspectRatioContainer/MarginContainer/BoxContainer/VBoxContainer/ScoreText"
+@onready var coins: Node = %Coins
+@export var win_canvas: PackedScene
 
+func _ready() -> void:
+	score = coins.get_child_count()
+	score_text.text = "Score: {score}".format({"score": str(score)})
 
 func add_point():
-	score += 1
+	score = coins.get_child_count()
 	score_text.text = "Score: {score}".format({"score": str(score)})
